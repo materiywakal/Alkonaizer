b6695e70ac0d914e02a23a52eb95ce1a9b037884
Alexander Sizov <kpeuc2000@gmail.com>
2019-08-21 11:48:02 +0300

    Some work

---
+++ Assets/Scripts/GameObjectScripts/AlchoholDialogScript.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AlchoholDialogScript : MonoBehaviour
+{
+    public AlchoholModel Alchohol;
+    
+    [SerializeField] public GameObject VolumeInput;
+    [SerializeField] public GameObject PercentageInput;
+
+    private void Start()
+    {
+        Alchohol = new AlchoholModel();
+    }
+
+    private void LoadModel()
+    {
+        // Take AlchoholModel from logical entity
+    }
+
+    public void Save()
+    {
+        // Send AlchoholModel to logical entity
+
+        DestroySelf();
+    }
+
+    public void ParseVolume()
+    {
+        float volume = Single.Parse(VolumeInput.GetComponent<InputField>().text.Replace('.', ','));
+        if (volume >= 0)
+        {
+            Alchohol.Volume = volume;
+        }
+        else
+        {
+            VolumeInput.GetComponent<InputField>().text = null;
+        }
+    }
+
+    public void ParsePercentage()
+    {
+        float percentage = Single.Parse(PercentageInput.GetComponent<InputField>().text.Replace('.', ','));
+        if (percentage >= 0 && percentage <= 100)
+        {
+            Alchohol.Percentage = percentage;
+        }
+        else
+        {
+            PercentageInput.GetComponent<InputField>().text = null;
+        }
+    }
+
+    public void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/GameObjectScripts/PeopleDialogScript.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PeopleDialogScript : MonoBehaviour
+{
+    public PeopleModel People;
+
+    [SerializeField] public GameObject MCheckBox;
+    [SerializeField] public GameObject HeightInput;
+    [SerializeField] public GameObject WeightInput;
+
+    private void Start()
+    {
+        People = new PeopleModel();
+    }
+
+    private void LoadModel()
+    {
+        // Take PeopleModel from logical entity
+    }
+
+    public void Save()
+    {
+        // Send PeopleModel to logical entity
+
+        DestroySelf();
+    }
+
+    public void ParseHeight()
+    {
+        float height = Single.Parse(HeightInput.GetComponent<InputField>().text.Replace('.', ','));
+        if (height >= 30 && height <= 300)
+        {
+            People.Height = height;
+        }
+        else
+        {
+            HeightInput.GetComponent<InputField>().text = null;
+        }
+    }
+
+    public void ParseWeight()
+    {
+        float weight = Single.Parse(WeightInput.GetComponent<InputField>().text.Replace('.', ','));
+        if (weight >= 20 && weight <= 300)
+        {
+            People.Weight = weight;
+        }
+        else
+        {
+            WeightInput.GetComponent<InputField>().text = null;
+        }
+    }
+
+    public void ParseGender()
+    {
+        People.IsMale = MCheckBox.GetComponent<Toggle>().isOn;
+    }
+
+    public void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+}
